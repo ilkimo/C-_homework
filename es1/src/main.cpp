cfa29d63c6f2abc6e5f2d52afab848405f795f85
ilkimo <vibertikim@yahoo.it>
2022-03-18 01:30:31 +0100

    chore: add es1 input validation

--- es1/src/main.cpp
+++ es1/src/main.cpp
@@ -26,9 +26,32 @@ void echo_arguments(const int argc, char** argv, ostream& outs=std::cout)
     }
 }
 
+/**
+ * Validates input. This function does NOT check if input is numerical. In case of alphanumeric args, atoi() will return zero
+ * and the program will execute normally.
+ * @param argc number of arguments
+ * @param argv 2D array of C-style (null terminated) strings
+ * @throws invalid_argument if based on the problem criteria, the given args are not correct
+ */
+void validate_args(const int argc, char** argv) {
+    if(argc != 5) {
+        throw invalid_argument("\033[1;31m At least 4 numbers are needed as arguments! (x, y, min, max) \033[0m");
+    }
+
+    if(atoi(argv[3]) > atoi(argv[4])) {
+        throw invalid_argument("\033[1;31m min can't be greater than max! Fix program args \033[0m");
+    }
+}
+
 int main(int argc, char** argv)
 {
     echo_arguments(argc, argv);
+
+    try {
+        validate_args(argc, argv);
+    } catch(std::invalid_argument const& exception) {
+        cout << exception.what() << endl;
+    }
 
     return 0;
 }
